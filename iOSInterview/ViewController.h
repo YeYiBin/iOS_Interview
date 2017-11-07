e2269d9c2a4198431f3dac9b94caf31f844e40b9
DeLongYang <1157013577@qq.com>
2017-11-07 20:03:31 +0800

    1.0  初始化工程

---
+++ iOSInterview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSInterview
+//
+//  Created by meitianhui2 on 2017/11/7.
+//  Copyright © 2017年 DeLongYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSInterview/ViewController.m
@@ -0,0 +1,122 @@
+//
+//  ViewController.m
+//  iOSInterview
+//
+//  Created by meitianhui2 on 2017/11/7.
+//  Copyright © 2017年 DeLongYang. All rights reserved.
+/*
+    主要演示 assing strong copy retain weak 的区别
+ */
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark ---- 简单测试
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
